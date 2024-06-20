e7df92f158235f1faf3374312c43609b5f47ed17
patrickslf <pslf2001@gmail.com>
2024-06-19 22:30:31 -0300

    Pastas realocadas

---
+++ Exercicios/19- idade.py
@@ -0,0 +1,12 @@
+'''
+Descrição: Faça um programa que você insere o ano 
+            de nascimento e mostre a idade.
+
+'''
+
+idade = 0
+ano_atual = 2024
+ano = int(input("Digite o ano do seu nascimento: "))
+
+idade = ano_atual - ano
+print (idade)
