64394b218e60a640418fd151c80d23969ed68383
unknown <pslf2001@gmail.com>
2024-06-27 22:33:40 -0300

    Revisao codigo 21

--- Exercicios/21 - velha.py
+++ Exercicios/21 - velha.py
@@ -1,33 +1,60 @@
 '''
-Descição    : Faça um jogo da velha.
-
+Descrição: Faça um Jogo da velha
 '''
-velha = [ ['1', '2', '3'],
-          ['4', '5', '6'],
-          ['7', '8', '9']
-        ]
 
+idosa = [['1','2','3'],
+         ['4','5','6'],
+         ['7','8','9']]
 rodadas = 0
-jogador1 = 'X'
-while rodadas <9:
-    posicao = input(f'Jogador {jogador1} Selecione uma posição:')
+jogador = 'X'
+def mostrar_vencedor(vencedor):#vencedor escopo local da função
+    print('Temos um vencedor!', vencedor)
+    
+while rodadas < 9:
+    linhacomplementar = '-------------'
+    posicao = input(f'jogador {jogador} escolha uma posição:')
     posicao_encontrada = False
-    linhacomplementar = '------  -----  ------'  
-    for linha in range (3):
-        linhacompleta = ''
-        for coluna in range (3):
-            if posicao == velha[linha][coluna]:
-                velha[linha][coluna] = jogador1 
+    for linha in range(3):
+        linha_completa = ''
+        for coluna in range(3):
+            if posicao == idosa[linha][coluna]:
+                idosa[linha][coluna] = jogador
                 posicao_encontrada = True
-            #linhacompleta = linhacompleta + clsvelha [linha][coluna] + '|'
-            linhacompleta += '   |' + velha [linha][coluna] + '|'
-        print(linhacompleta)
+            linha_completa += idosa [linha][coluna] + ' | '
+        print(f' {linha_completa}')
         print(linhacomplementar)
     if posicao_encontrada == True:
         rodadas = rodadas + 1
-        if jogador1 == 'X':
-         jogador1 = 'O'
+        if jogador == 'X':
+            jogador = 'O'
         else:
-            jogador = 'X' 
+            jogador = 'X'
     else:
-         print('Posição não encontrada')
+        print('posicao não encontrada')
+
+    if (idosa [0][0] == idosa [0][1] == idosa [0][2]): #linha 1
+        mostrar_vencedor(idosa [0][0])
+        break
+    elif(idosa [1][0] == idosa [1][1] == idosa [1][2]): # linha 2
+        mostrar_vencedor(idosa [1][0])
+        break
+    elif(idosa [2][0] == idosa [2][1] == idosa [2][2]): # linha 3
+        mostrar_vencedor(idosa [2][0]) 
+        break    
+    elif(idosa [0][0] == idosa [1][0] == idosa [2][0]): #coluna 1
+        mostrar_vencedor(idosa [0][0]) 
+        break
+    elif(idosa [0][1] == idosa [1][1] == idosa [2][1]): #coluna 2
+        mostrar_vencedor(idosa [0][1])
+        break
+    elif(idosa [0][2] == idosa [1][2] == idosa [2][2]): #coluna 3
+        mostrar_vencedor(idosa [0][2]) 
+        break
+    elif(idosa [0][0] == idosa [1][1] == idosa [2][2]): #diagonal 1
+        mostrar_vencedor(idosa [0][0])
+        break    
+    elif(idosa [0][2] == idosa [1][1] == idosa [2][0]): #diagonal 1
+        mostrar_vencedor(idosa [0][2])
+        break 
+else:
+    print('Velha, tente novamente!')
